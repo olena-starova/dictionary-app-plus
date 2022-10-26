db7cb8561cdef2a9159af20f60cd0df7a2686e1c
olena-starova <iverra@ukr.net>
2022-10-26 19:53:18 +0300

    Additional Styling

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
+import Meaning from "./Meaning";
 import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
     return (
-      <div className="Result">
+      <div className="Results">
         <section>
           <h2>{props.results.word}</h2>
           {props.results.phonetics.map(function (phonetic, index) {
